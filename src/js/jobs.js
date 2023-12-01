30e3c2bc0c8645a479af5650cf47ddb3bd8fbba6
YahirAdrian <yahiradrian7948@gmail.com>
2023-12-01 11:36:59 -0500

    Topic page

---
+++ src/js/jobs.js
@@ -0,0 +1,10 @@
+// const vocabularyCards = document.querySelectorAll('.vocabulary-card')
+
+// vocabularyCards.forEach((card)=>{
+//     card.addEventListener('mouseenter', flipCard(card));
+//     card.addEventListener('mouseleave', flipCard(card))
+// })
+
+// function flipCard(card){
+//     console.log(card)
+// }
